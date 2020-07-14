18dd74de6bfcf0453921c97a4a3f06f412229098
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-14 18:45:40 +0530

    product image field renderd

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
@@ -17,9 +18,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class Order(models.Model):
