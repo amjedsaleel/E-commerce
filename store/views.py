2f2220ba31faa4554d7a18498d2eecadad2c6747
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-14 17:12:34 +0530

    product rendered to store page

--- store/views.py
+++ store/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import *
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
